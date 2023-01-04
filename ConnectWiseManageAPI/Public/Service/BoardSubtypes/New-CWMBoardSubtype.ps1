e0a3bfc9f013785cf705bee6d3824928719de1b7
John Rogers <jrogers@winsorgroup.com>
2023-01-04 15:18:00 -0600

    Add New, Update, Remove functions for BoardSubtype

---
+++ ConnectWiseManageAPI/Public/Service/BoardSubtypes/New-CWMBoardSubtype.ps1
@@ -0,0 +1,20 @@
+﻿function New-CWMBoardSubtype {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSShouldProcess', '', Justification = 'Used by sub-function')]
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact='Medium')]
+    param(
+        [Parameter(Mandatory = $true)]
+        [Alias('boardId')]
+        [int]$parentId,
+        [Parameter(Mandatory = $true)]
+        [ValidateLength(1,50)]
+        [string]$name,
+        [Parameter(Mandatory = $true)]
+        [int[]]$typeAssociationIds,
+        [switch]$inactiveFlag,
+        [switch]$addAllTypesFlag,
+        [switch]$removeAllTypesFlag
+    )
+
+    $Endpoint = "/service/boards/$($parentId)/subtypes"
+    Invoke-CWMNewMaster -Arguments $PsBoundParameters -Endpoint $Endpoint
+}
---
+++ ConnectWiseManageAPI/Public/Service/BoardSubtypes/Remove-CWMBoardSubtype.ps1
@@ -0,0 +1,14 @@
+﻿function Remove-CWMBoardSubtype {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSShouldProcess', '', Justification = 'Used by sub-function')]
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact='Medium')]
+    param(
+        [Parameter(Mandatory = $true)]
+        [Alias('boardId')]
+        [int]$parentId,
+        [Parameter(Mandatory = $true)]
+        [int]$id
+    )
+
+    $Endpoint = "/service/boards/$($parentId)/subtypes/$($id)"
+    Invoke-CWMRemoveMaster -Endpoint $Endpoint
+}
